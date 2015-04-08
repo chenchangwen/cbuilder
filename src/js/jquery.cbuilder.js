b2fc4717de902f5bd6ac512c04bcb48c2396532e
chenchangwen <21108589@qq.com>
2015-04-09 03:44:19 +0800

    实现加载弹窗插件,修正触发事件方法 Signed-off-by: chenchangwen <21108589@qq.com>

--- src/js/jquery.cbuilder.js
+++ src/js/jquery.cbuilder.js
@@ -16,41 +16,43 @@
         height: "100%",
         plugins: ["upload"],
         prefix: "cbuilder",
-        clsToolbar: ".cb-toolbar",
-        clsBody: ".cb-body",
         tpl: {
             toolbar: "<div class=\"cb-toolbar\"></div>",
-            toolbar_button: "<div class=\"cb-button-wrap\"><button class=\"cb-button\">{name}</button></div>",
+            toolbar_button: "<div class=\"cb-button-wrap\"><button class=\"cb-button {clsname}\">{name}</button></div>",
             body: "<div class=\"cb-body\"></div>"
         },
         onComplete: false
     };
+
+    function currentScriptPath() {
+        var scripts = document.querySelectorAll('script[src]');
+        var currentScript = scripts[scripts.length - 1].src;
+        var currentScriptChunks = currentScript.split('/');
+        var currentScriptFile = currentScriptChunks[currentScriptChunks.length - 1];
+        return currentScript.replace(currentScriptFile, '');
+    }
+
+    var clsToolbar = ".cb-toolbar",
+        clsBody = ".cb-body",
+        clsButton = '.cb-button',
+        clsContainer = '.cb-container',
+        basePath = currentScriptPath();
+
 
 
     var cbuilder = function(element, options) {
         this.options = $.extend({}, defaults, options);
         this.$element = $(element);
         this.strucView();
-        this.strucEvents();
     };
-
-    //private Method
-    function strucPrivateEvents() {
-        this._trigger = function(event, callback) {
-            this.$element.trigger(event);
-            if (callback) {
-                callback.call(this.$element);
-            }
-        };
-    }
-    
+ 
 
     cbuilder.prototype = {
         strucView: function() {
             var that = this;
             var view = {
                 appendHtml: function() {
-                    that.$element.addClass("cb-container")
+                    that.$element.addClass('cb-container')
                         .wrap(that.options.tpl.container)
                         .append(that.options.tpl.toolbar + that.options.tpl.body);
                 },
@@ -62,27 +64,59 @@
                         '../../vendor/dropzone/dist/dropzone.js'
                     ];
                     for (var i = 0; i < vendors.length; i++) {
-                        $.getScript(vendors[i]);
+                        var vendor = vendors[i];
+                        if (vendor.indexOf('css') >= 0) {
+                            var cssLink = $("<link rel='stylesheet' type='text/css' href='" + vendor + "'>");
+                            $("head").append(cssLink);
+                        } else {
+                            $.getScript(vendor);
+                        }
                     }
                 },
-                //加载插件
+                //加载plugin
                 loadPlugins: function () {
                     var len = that.options.plugins.length;
                     for (var i = 0; i < len; i++) {
                         var name = that.options.plugins[i];
-                        $.getScript('src/js/plugins/' + name + '/' + 'plugin' + '.js', function () {
-                            var plugin = cbuilderPluginObj();
-                            that.$element.find(that.options.clsToolbar).append(that.options.tpl.toolbar_button.replace(/\{name\}/, plugin.name));
+                        $.get('src/js/plugins/' + name + '/' + 'plugin' + '.js', function () {
+                            //执行动态函数,并获取plugin对象
+                            var plugin = getCbuilderPlugin();
+                            //替换为plugin名字
+                            that.$element.find(clsToolbar).
+                                append(that.options.tpl.toolbar_button.
+                                    replace(/\{name\}/, plugin.name).
+                                    replace(/\{clsname\}/, plugin.className)
+                                );
+                            var pluginbtn = that.$element.find('.' + plugin.className);
+                            that._trigger('', plugin.onDomReady);
+
+                            pluginbtn.on('click', function () {
+                                if (plugin.type === 'iframe') {
+                                    $.fancybox.open({
+                                        href: basePath + 'plugins/' + name + '/plugin.html',
+                                        type: 'iframe',
+                                        padding: 5,
+                                        scrolling: 'no',
+                                        fitToView: true,
+                                        width: plugin.width || 610,
+                                        height: plugin.height || 300,
+                                        autoSize: false,
+                                        closeClick: false
+                                    });
+                                    that._trigger('', plugin.onClicked);
+                                }
+                            });
                         });
-                        //按需加载插件
-                        //that.$element.find(that.options.clsToolbar).append(that.options.tpl.toolbar_button.replace(/\{name\}/,'asdf'));
                     }
                 },
-                bindEvents: function() {
-                    that.$element.find('.cb-button').on("click", function () {
-                        alert('123');
-                        //alert('123213');
-                    });
+                bindEvents: function () {
+//                    $(clsContainer).delegate(clsButton, 'click', function () {
+//                        $.fancybox.open({
+//                            href: 'iframe.html',
+//                            type: 'iframe',
+//                            padding: 5
+//                        });
+//                    });
                 },
                 struc: function() {
                     this.appendHtml();
@@ -93,8 +127,11 @@
             };
             view.struc();
         },
-        strucEvents: function() {
-            strucPrivateEvents.call(this);
+        _trigger: function(event, callback) {
+            this.$element.trigger(event);
+            if (callback) {
+                callback.call(this.$element);
+            }
         },
         show: function() {
 
--- src/js/plugins/upload/plugin.js
+++ src/js/plugins/upload/plugin.js
@@ -1,6 +1,16 @@
-﻿function cbuilderPluginObj() {
+﻿function getCbuilderPlugin() {
     var exports = {
-        name: "上传图片"
+        name: "上传图片",
+        className: 'cb-upload',
+        onDomReady: function() {
+            //alert('上传图片callback');
+        },
+        onClicked: function () {
+//            alert('onClicked');
+        },
+        width: 600,
+        height: 300,
+        type:'iframe'
     }
     return exports;
 }
